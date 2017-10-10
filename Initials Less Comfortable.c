d58e0a3e5223263075a6f8ab5e97aa066b9b54ed
Liu Jiawei <carlisle_liu@hotmail.com>
2017-10-10 10:40:33 +0800

    Create Initials Less Comfortable.c

---
+++ Initials Less Comfortable.c
@@ -0,0 +1,25 @@
+```
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{
+    // get user's name
+    string name = get_string();
+    
+    // iterate through the username
+    for (int i = 0, n = strlen(name); i < n; i++)
+    {
+        // identify the very first character or 1st character after a space in username
+        if (i == 0 || name[i - 1] == ' ')
+        {
+            // print the identified characters in uppercase
+            printf("%c", toupper(name[i]));
+        }
+    }
+    
+    printf("\n");
+}
+```
